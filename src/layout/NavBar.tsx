d8e0c0cfca8bc34258d6693d9b554628e8bf2bcf
Camilo Saez <camjasaez@gmail.com>
2022-09-03 18:25:10 -0400

    Añadido el navbar responsivo y con dropdown

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -2,20 +2,34 @@ import { Pokeball, PokeLogo, Sandwich } from '../utils/svg/svg';
 import { Link } from 'react-router-dom';
 import { selectCount } from '../features/favorites/favoriteSlice';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const NavBar = () => {
   const counterFav = useSelector(selectCount);
+  const [active, setActive] = useState(true);
+
+  const toggle = () => {
+    setActive(!active);
+  };
+
   return (
     <header className="relative z-10 min-h-[10vh] min-w-full bg-Third drop-shadow-lg">
-      <nav className=" mx-auto flex flex-wrap items-center justify-around">
-        <PokeLogo />
-        <button
-          type="button"
-          className="  ml-3 inline-flex items-center rounded-lg p-2 text-sm text-black  focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+      <nav className=" mx-auto flex flex-col flex-wrap items-center justify-around md:flex-row">
+        <div className="flex ">
+          <PokeLogo />
+          <button
+            type="button"
+            onClick={toggle}
+            className="  ml-3 inline-flex items-center rounded-lg p-2 text-sm text-black  focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+          >
+            <Sandwich />
+          </button>
+        </div>
+        <div
+          className={
+            active ? 'z-40 hidden w-auto md:block' : 'z-40 w-auto md:block'
+          }
         >
-          <Sandwich />
-        </button>
-        <div className=" z-40 hidden w-auto md:block">
           <ul className="  mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
             <li className="   text-base lg:text-lg">
               <Link to="/">Home</Link>
